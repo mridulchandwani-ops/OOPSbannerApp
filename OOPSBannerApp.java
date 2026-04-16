ff8d76f02d625da20d110ea2b4bb130829f3b1c9
mridulchandwani-ops <mridulchandwani@gmail.com>
2026-04-16 09:53:08 +0530

    UC1

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,6 @@
 public class OOPSBannerApp{
 	public static void main(String[]args)
 	{
-		System.out.println("***** ***** ***** *****");
-		System.out.println("* ** ** ** *");
-		System.out.println("* ** ** ** *");
-		System.out.println("* ** ** ** *");
-		System.out.println("* ** ** ** *");
-		System.out.println("***** ***** ***** *****");
+		System.out.println("OOPS");		
 }
 }
