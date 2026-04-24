1c8366e6bdc6da9170c5899cc1cd531fdcf2b7a1
mridulchandwani-ops <mridulchandwani@gmail.com>
2026-04-24 08:47:08 +0530

    added feature UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,18 @@
 public class OOPSBannerApp{
-	public static void main(String[]args)
-	{
-		System.out.println("***** ***** ***** *****");
-		System.out.println("* ** ** ** *");
-		System.out.println("* ** ** ** *");	
-		System.out.println("* ** ** ** *");	
-		System.out.println("* ** ** ** *");	
-		System.out.println("* ** ** ** *");
-		System.out.println("***** ***** ***** *****");
+	public static void main(String[] args) {
 
-}
+        // Banner stored in String array (centralized data structure)
+        String[] banner = {
+                "************************************",
+                "*                                  *",
+                "*        WELCOME TO OOPS APP       *",
+                "*                                  *",
+                "************************************"
+        };
+
+        // Using enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
